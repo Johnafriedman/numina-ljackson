3f75d4d45ddb2a499e3e8d1b77bfe352ccba3e8a
Johnafriedman <john@innovationsforlearning.org>
2020-11-04 14:47:28 -0600

    unversioned files commit

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Leavenworth Jackson Rubber Stamps</title>
+
+<!--[if IE 5]>
+<style type="text/css"> 
+/* place css box model fixes for IE 5* in this conditional comment */
+.twoColFixRtHdr #sidebar1 { width: 220px; }
+</style>
+<![endif]-->
+<!--[if IE]>
+<style type="text/css"> 
+/* place css fixes for all versions of IE in this conditional comment */
+.twoColFixRtHdr #sidebar1 { padding-top: 30px; }
+.twoColFixRtHdr #mainContent { zoom: 1; }
+/* the above proprietary zoom property gives IE the hasLayout it needs to avoid several bugs */
+</style>
+<![endif]-->
+<link href="favicon.ico" rel="shortcut icon" />
+<link href="main_lj.css" rel="stylesheet" type="text/css" />
+<meta name="Description" content="Leavenworth Jackson Rubber Stamps! Mail order and online purveyor of rubber stamps and supplies to the discriminating stamp art enthusiast since 1979." />
+<meta name="Keywords" content="rubber stamps, finely detailed rubber stamps, rubber stamp art, mail art, collage art, mixed media art, stamp pads, rainbow pads,scrapbook, Berkeley, California" />
+</head>
+
+<body class="twoColFixRtHdr">
+
+<div id="container">
+  <div id="header">
+    <div id="title"> 
+      <h1>Leavenworth Jackson Rubber Stamps</h1>
+    </div>
+    <div id="stampart3"><img src="images_lj/fragile_ship.jpg" alt="fragile ship stamp art" width="780" height="512" /></div>
+    <div id="navbar">
+      <h4>Home | <a href="pages/about.html">About</a> | <a href="pages/catalog.html">Catalog</a> | <a href="pages/artwork.html">Artwork</a> | <a href="pages/contact.html">Contact</a></h4>
+    </div>
+  </div>
+  <div id="sidebar1">
+    <h5>Viral Times</h5>
+    <p class="maintext">We continue to make and send  stamps while sheltering in place. Wishing you safe passage through the days ahead. Please take good care of yourself and others! </p>
+    <h5 class="twoColFixRtHdr">PayPal Invoice Option</h5>
+    <p class="maintext">Choose your stamps from our <a href="pages/catalog.html">online catalog</a> or from  <a href="https://mailchi.mp/fa587448a5c3/rubber-stamps-2712008">any of our newsletters</a> or even from the depths of your archived paper catalogs. <a href="pages/contact.html">Contact us here</a> with any questions plus the particulars of your order. Once we gather goods and shipping details, we will add up the total  and email you a PayPal invoice  for secure  payment.</p>
+    <h5>Remember
+    </h5>
+    <p class="maintext">You have a natural grace and great consideration of others.</p>
+    <h5>Our Newsletter </h5>
+    <p class="maintext">We send out a newsy blast once or twice a year. <a href="pages/about.html">More about that here.</a></p>
+  </div>
+  <div id="mainContent">
+    <h2>Welcome...</h2>
+    <p class="maintext">Dear Fellow Rubber Stamp Enthusiasts,<br />
+During these    extraordinary times many folks are staying home, including us. That said, we continue to fire up the in-house vulcanizers  to make stamps. As long as the Postal Service delivers, we are shipping  orders promptly while minding the rules of social distancing. Also, we wanted you to know that we now have  PayPal invoicing for secure online payments. <a href="pages/contact.html">Contact us</a> to open the process via email. Once you have written up the particulars of your stamp and supply desires, we can iron out  details and   arrive at your invoice total. Soon your stamps will be on their way. Meanwhile, stay comfortable and safe. May good health and humor be yours for the duration. Slow down, take a deep breath and consider indulging in a trip to the Rubber Zone.</p>
+    <div id="photodiv"><img src="images_lj/Solstice-copy.jpg" alt="solstice sun art" width="200" height="209" /></div>
+    <h3>Love Letters, Journals,</h3>
+    <p class="maintext"> scrapbooks,  cards, social  posts, notes, wordplay and oddball  scribblings await your artfully inspired adornment. The Rubber Zone is nothing other than your own natural, infinitely flexible capacity to visualize, recognize and shape creative opportunities. </p>
+    <h3>Manifest Pictorial Zest</h3>
+     <p class="maintext"> Tune in to the pictorial play of your own wild mind. Dust off your stamps and relish some quiet time either on your own or with others of your household for a deep dive into uncharted territory on paper. Conjure up outflows of illustrated letters, notes, cards and posts to Dear Ones near and far. Though in-person visits  are on hold for now, your tangible messages will convey kindness and care in this age of  high-speed overwhelm. Share encouraging words. Times are dicey, but this too will pass.</p>
+     <p class="maintext">&nbsp;</p>
+  </div>
+<div id="footer">
+	  <p align="center"><span class="footertext">©2008-20 Leavenworth Jackson • Box 9988 • Berkeley • CA 94709</span></p>
+  </div>
+<!-- end #container --></div>
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+var pageTracker = _gat._getTracker("UA-4680989-1");
+pageTracker._initData();
+pageTracker._trackPageview();
+</script>
+</body>
+</html>
